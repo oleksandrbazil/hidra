45d682637ce9ed80fcb0ce6b61bb2bbfb75d800f
Oleksandr Bazil <oleksandr@soften-it.com>
2016-05-16 18:05:48 +0300

    add enum work statuses

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,6 @@
 class Work < ApplicationRecord
   belongs_to :project
   belongs_to :developer
+  enum status: [ :default, :pending, :in_progress, :done ]
+
 end
---
+++ db/migrate/20160516150150_add_works_status.rb
@@ -0,0 +1,5 @@
+class AddWorksStatus < ActiveRecord::Migration[5.0]
+  def change
+    add_column :works, :status, :integer, default: 0
+  end
+end
