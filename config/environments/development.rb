84219ed4eb7fd2518dbcae0cfb77188eef2d325b
Oleksandr Bazil <oleksandrbazil@gmail.com>
2016-05-19 13:44:11 +0300

    email delivery settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,4 +50,13 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  ActionMailer::Base.smtp_settings = {
+      address:"smtp.gmail.com",
+      port: 587,
+      domain: 'gmail.com',
+      user_name: '',
+      password: '',
+      authentication: 'plain',
+      enable_starttls_auto: true
+  }
 end
