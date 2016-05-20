e676fb73166e018096997337470d9d12066dc126
Oleksandr Bazil <oleksandrbazil@gmail.com>
2016-05-20 14:01:52 +0300

    change redirect url

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -29,7 +29,7 @@ class ClientsController < ApplicationController
 
     respond_to do |format|
       if @client.save
-        format.html { redirect_to @client, notice: 'Client was successfully created.' }
+        format.html { redirect_to clients_url, notice: 'Client was successfully created.' }
         format.json { render :show, status: :created, location: @client }
       else
         format.html { render :new }
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -32,7 +32,7 @@ class WorksController < ApplicationController
     @work.user_id = current_user.id if current_user
     respond_to do |format|
       if @work.save
-        format.html { redirect_to @work, notice: 'Work was successfully created.' }
+        format.html { redirect_to works_url, notice: 'Work was successfully created.' }
         format.json { render :show, status: :created, location: @work }
       else
         format.html { render :new }
